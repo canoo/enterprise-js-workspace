042033b5cb0ea4960f9905f3f038800eec273de4
Andreas Hölzl <andy.woodly@googlemail.com>
2013-07-16 13:29:25 +0200

    work in progress

---
+++ exercise-grunt/src/app/controller/Controller.js
@@ -0,0 +1,34 @@
+define(function () {
+
+    var Controller = function(options){
+        this.inputView = options.input;
+        this.detailView = options.detail;
+        this.model = options.model;
+
+        this.initialize();
+    };
+
+    Controller.prototype.initialize = function() {
+        var me = this;
+
+        // add enter key listener
+        this.inputView.addKeyListener(function (input) {
+            me.model.addItem({name: input});
+            me.detailView.render({ items: me.model.getItems() });
+        });
+
+        // add button click listener
+        this.inputView.addClickListener(function (input) {
+            me.model.addItem({name: input});
+            me.detailView.render({ items: me.model.getItems() });
+        });
+
+        // add button click listener
+        this.detailView.addChangeListener(function (value, index) {
+            me.model.updateItem({name: value}, index);
+            me.detailView.render({ items: me.model.getItems() });
+        });
+    };
+
+    return Controller;
+});
