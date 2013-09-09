5798da13de1e4015c7c050826c0734c219824a1e
Oleg Buss <olegbuss@gmail.com>
2013-09-09 21:07:20 +0200

    added gruntfile comments for build automation slides#

--- 40-exercise-grunt/Gruntfile.js
+++ 40-exercise-grunt/Gruntfile.js
@@ -1,10 +1,27 @@
+/**
+ * Typical Gruntfile.js script
+ *
+ * Define a public function with the grunt parameter
+ * this method will be invoked by the grunt task runner
+ *
+ * For basic task configuration see: http://gruntjs.com/configuring-tasks
+ *
+ */
 module.exports = function (grunt) {
 
-    var buildDir = 'build/';
+    /**
+     * you can setup variables used by the build script
+     */
+    var buildDir = 'build/dist/';
+    var debugDir = 'build/debug/';
     var cssDir = buildDir + 'resources/css/';
 
-    // Project configuration.
+    // Project configuration
     grunt.initConfig({
+
+            /**
+             * Read contents of the package.json file
+             */
             pkg: grunt.file.readJSON('package.json'),
 
 
@@ -23,6 +40,15 @@ module.exports = function (grunt) {
                 }
             },
 
+            /**
+             * This task uses r.js the requirejs optimizer cli tool to build requirejs projects
+             *
+             * Task Docs: https://npmjs.org/package/grunt-contrib-requirejs
+             *
+             * For a full list of r.js build options see:
+             *
+             * https://github.com/jrburke/r.js/blob/master/build/example.build.js
+             */
             requirejs: {
                 compile: {
                     options: {
@@ -35,12 +61,22 @@ module.exports = function (grunt) {
                          * config.js which is used inside index.html
                          */
                         mainConfigFile: "src/config.js",
-                        optimize      : "uglify",
-                        out           : buildDir + "app.js"
+                        optimize      : "uglify", // or google closure compiler is also available
+
+                        /**
+                         * e.g use the package.json file to get the name of the application
+                         */
+                        out           : buildDir + "/<%= pkg.bundleName %>.js"
                     }
                 }
             },
 
+            /**
+             *
+             * Minifies CSS files
+             *
+             * Task Docs: https://npmjs.org/package/grunt-contrib-cssmin
+             */
             cssmin: {
                 compress: {
                     files: [
@@ -49,11 +85,25 @@ module.exports = function (grunt) {
                 }
             },
 
+            /**
+             * Removes all files in build folder
+             */
             clean: {
-                build: [ buildDir ]
+                build: [ 'build/' ]
             },
 
+            /**
+             * Task Docs: https://npmjs.org/package/grunt-contrib-copy
+             */
             copy: {
+
+                /**
+                 * Copy task "build" target options go here
+                 *
+                 * Call from the command line:
+                 *
+                 * $ grunt copy:build
+                 */
                 build: {
                     files: [
                         {
@@ -65,17 +115,56 @@ module.exports = function (grunt) {
                             dest  : buildDir
                         },
                     ]
+                },
+
+                /**
+                 * Copy task "build" target options go here
+                 *
+                 * Call from the command line:
+                 *
+                 * $ grunt copy:debug
+                 */
+                debug: {
+                    files: [
+                        {
+                            expand: true,
+                            cwd   : 'src/',
+                            src   : [
+                                '**'
+                            ],
+                            dest  : debugDir
+                        },
+                    ]
+                }
+            },
+
+            //
+            watch: {
+                cssmin: {
+                    files: [ 'src/resources/**/*.css' ],
+                    tasks: ['cssmin']
                 }
             }
         }
     );
 
+    // load additional grunt tasks provided by the grunt community
+    // the dependencies are defined inside the package.json file
+
+    // "contrib" tasks are maintained and developed by the grunt team
     grunt.loadNpmTasks('grunt-contrib');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-requirejs');
 
-    // Default task.
+    // Default task which is executed if grunt is called without task arguments
     grunt.registerTask('default', 'build'.split(' '));
+
+    // you can define simple tasks which are composed of other tasks
     grunt.registerTask('build', 'cssmin requirejs uglify copy:build'.split(' '));
+
+    grunt.registerTask('debug', 'copy:debug'.split(' '));
+
+
+    // for more information on how to implement own tasks see http://gruntjs.com/creating-tasks
 };
